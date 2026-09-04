9650cd95d64dcb2781a2653a9598dd1393d21fbd
dalfonsobermudez <dalfonsobermudez@gmail.com>
2026-09-04 10:05:47 -0400

    Enhance templates and URLs for improved layout and navigation consistency

--- proyectoyane/proyectoyaneblog/urls.py
+++ proyectoyane/proyectoyaneblog/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from proyectoyaneblog import views
 
 urlpatterns = [
-    path('', views.blog, name='Blog'),
-    path('category/<slug:category_slug>/', views.category_posts, name='PostsByCategory'),
-    path('<slug:post_slug>/', views.post_detail, name='PostDetail'),
+    path("", views.blog, name="Blog"),
+    path(
+        "category/<slug:category_slug>/",
+        views.category_posts,
+        name="PostsByCategory",
+    ),
+    path("<slug:post_slug>/", views.post_detail, name="PostDetail"),
 ]
--- proyectoyane/proyectoyaneshop/urls.py
+++ proyectoyane/proyectoyaneshop/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from proyectoyaneshop import views
 
 urlpatterns = [
-    path('', views.shop, name="Shop"),
-    path('category/<slug:category_slug>/', views.category_products, name="ProductsByCategory"),
-    path('<str:product_slug>/', views.product_details, name="ProductDetails"),
+    path("", views.shop, name="Shop"),
+    path(
+        "category/<slug:category_slug>/",
+        views.category_products,
+        name="ProductsByCategory",
+    ),
+    path("<str:product_slug>/", views.product_details, name="ProductDetails"),
 ]
