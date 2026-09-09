059bd6f233bb9626b03c457249532eb3b5fba1ca
dalfonsobermudez <dalfonsobermudez@gmail.com>
2026-09-09 14:57:05 -0400

    changed installed apps for cloudinary in settings.py

--- proyectoyane/proyectoyane/settings.py
+++ proyectoyane/proyectoyane/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary',
-    'cloudinary_storage',
+    'cloudinary_storage.apps.CloudinaryStorageConfig',
     'proyectoyaneapp',
     'proyectoyaneshop',
     'proyectoyanecontact',
